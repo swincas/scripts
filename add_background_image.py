5d125a58bdeb4c460dc96332c5d4b23d7306a59c
SaraWebb <webb.sara.a@gmail.com>
2018-07-17 21:20:48 +1000

    Updated scripts with comments for general use

--- add_background_image.py
+++ add_background_image.py
@@ -3,16 +3,22 @@ from PIL import Image
 import os 
 
 
-backgroundimage = '/Users/swebb/Documents/DWF/sky_plots/DWF_world_plot/images/stars_all_labels.png'
+## use this script to add a background image behind transparent globe image
 
+
+# path of image you want to use as your background
+backgroundimage = 'Path'
+
+# Function to merge background image and globe images from path
 def make_im(path):
-	for filename in os.listdir(path):
-		if filename.startswith('DWF'):
-			background = Image.open(backgroundimage)
-			foreground = Image.open(path + filename)
+    for filename in os.listdir(path):
+        if filename.startswith('DWF'): # use these for specfic start name or filename.endswith for specfic ext. ending
+            background = Image.open(backgroundimage) # open the background image
+            foreground = Image.open(path + filename) # open each globe image
 	
-			background.paste(foreground, (-1, -1), foreground)
-			background.save('/Users/swebb/Documents/DWF/sky_plots/DWF_world_plot/images/globe_final/MOD_all_label_' + filename)
+            background.paste(foreground, (-1, -1), foreground) # pastes the globe image on the background
+            background.save('/Users/swebb/Documents/DWF/sky_plots/DWF_world_plot/images/globe_final/MOD_all_label_' + filename) # save the mergered image to chosen dir with your chosen name
 
-testing = make_im('/Users/swebb/Documents/DWF/sky_plots/DWF_world_plot/images/globe_final/')
+#-------------- Run the function for your choosen path --------------------------- #
+make_im('/Users/swebb/Documents/DWF/sky_plots/DWF_world_plot/images/globe_final/')
 
--- make_globe_video.py
+++ make_globe_video.py
@@ -3,25 +3,25 @@ import os
 from PIL import Image
 imageio.plugins.ffmpeg.download()
 
-#field = 'Antlia'
-#cand = 100
 
+#---=== choose path of images you want to merged together into a video --- 
 path = '/Users/swebb/Documents/DWF/sky_plots/DWF_world_plot/images/globe_final/'
 
+
+# --- create array of all files you want to merger from path ----- 
 filelist = []
 files = []
+
 for filename in os.listdir(path):
-	if filename.startswith('MOD_all_label_'):
-		files.append(filename)
+	if filename.startswith('MOD_all_label_'): # or use .endswith to choose ext. type 
+		files.append(filename) # add filename to the array 
 		
-files.sort()
-#print(files)
+files.sort() # sort the files into accessending order 
 
-writer = imageio.get_writer('/Users/swebb/Documents/DWF/sky_plots/DWF_world_plot/images/DWF_World_15fps.mp4', fps = 15)
+writer = imageio.get_writer('/Users/swebb/Documents/DWF/sky_plots/DWF_world_plot/images/DWF_World_15fps.mp4', fps = 15) #make path and name of video you want to create and choose frames per second with fps. 
 
+# --- Create the video 
 for im in files:
-	test = path + im
-	print(test)
-	writer.append_data(imageio.imread(path + im))
+	writer.append_data(imageio.imread(path + im)) 
 writer.close() 
 
